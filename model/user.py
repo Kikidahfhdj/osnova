77b03b1c2a8568ad91120434b92f32db5b42a254
kiki <158156491+Kikidahfhdj@users.noreply.github.com>
2024-08-26 20:20:33 +0300

    26 августа user.py

--- model/user.py
+++ model/user.py
@@ -1,3 +1,13 @@
-class User:
-    def __init__(self, username: str):
-        self.username = username
+import warnings
+from typing import Tuple
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+from sklearn.metrics import roc_auc_score
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import validation_curve, learning_curve
